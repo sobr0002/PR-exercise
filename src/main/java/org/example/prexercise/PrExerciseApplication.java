369ae66248cdc4ee934437eb678edd6e042adb04
Sofie Broman <sofierytter@hotmail.com>
2025-02-18 15:07:24 +0100

    Initial commit

---
+++ src/main/java/org/example/prexercise/PrExerciseApplication.java
@@ -0,0 +1,13 @@
+package org.example.prexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrExerciseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/prexercise/PrExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.prexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
